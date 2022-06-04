cde27a053a3f4cacfff686c12bf28639cba5d2a7
Alexandr Hardcore'ov <sharkov.as88@gmail.com>
2022-06-04 23:54:19 +0300

    Homework about smarter superhero. last

--- Superheroes.py
+++ Superheroes.py
@@ -1,16 +1,32 @@
-class YaUploader:
-    def __init__(self, token: str):
-        self.token = token
+import requests
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+
+class Superheroes:
+    def __init__(self):
+        with open('token.txt', 'r') as token_file:
+            self.token = token_file.read().strip()
+
+    def superhero(self, name):
+        url = f'https://superheroapi.com/api/{self.token}/search/{name}'
+        res = requests.get(url)
+        return res
+
+    def superheroes_intelligence(self, names_of_superheroes):
+        superheroes_intelligence = {}
+        for name in names_of_superheroes:
+            superheroes_intelligence[name] = self.superhero(name).json()['results'][0]['powerstats']['intelligence']
+        return superheroes_intelligence
+
+    def highest_intelligence(self):
+        highest_int = max([int(i) for i in list(self.superheroes_intelligence(name_of_superheroes).values())])
+        for name, intelligence in self.superheroes_intelligence(name_of_superheroes).items():
+            if int(intelligence) == highest_int:
+                return f'Самый высокий интелект - {highest_int} у персонажа: {name}'
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+    name_of_superheroes = ['Hulk', 'Captain America', 'Thanos']
+    superheroes = Superheroes()
+    superheroes.superheroes_intelligence(name_of_superheroes)
+    highest_intelligence = superheroes.highest_intelligence()
+    print(highest_intelligence)
